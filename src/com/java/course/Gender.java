b24aa985d6c691b5b6f0104df1504db93b9f234b
OtorovDmitriy <otorov_dmitriy@iMac-Dmitrij.local>
2019-09-16 22:50:58 +0300

    cw-2 v.1

---
+++ src/com/java/course/Gender.java
@@ -0,0 +1,6 @@
+package com.java.course;
+
+public enum Gender {
+    MAN,
+    WOMAN
+}
--- src/com/java/course/Main.java
+++ src/com/java/course/Main.java
@@ -3,6 +3,18 @@ package com.java.course;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person Tom = new Person("Tom", 22, Gender.MAN);
+        Person Mary = new Person("Mary", 20, Gender.WOMAN);
+        Person Kate = new Person("Kate", 21, Gender.WOMAN);
+        Person Mike = new Person("Mike", 25, Gender.MAN);
+
+        Tom.marry(Mary);
+        Tom.marry(Kate);
+        Mary.marry(Mike);
+
+        System.out.println(Tom);
+        System.out.println(Mary);
+        System.out.println(Kate);
+        System.out.println(Mike);
     }
 }
