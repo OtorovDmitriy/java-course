74be577e0cde27e75548bee270aa6d14ab95f17a
Dmitriy Otorov <otorov_dmitri@MacBook-Pro-otorov-dmitri.local>
2019-09-30 00:40:03 +0300

    hw-3 v.1

--- src/com/java/course/Main.java
+++ src/com/java/course/Main.java
@@ -3,6 +3,12 @@ package com.java.course;
 public class Main {
 
     public static void main(String[] args) {
-        Solution.main(args);
+        Map<String, Integer> map = new Map<>();
+        map.put("hello", 1);
+        map.put("world", 2);
+        System.out.println(map.get("hello"));
+        System.out.println(map.remove("hello"));
+        System.out.println(map.contains("world"));
+        System.out.println(map.size());
     }
 }
